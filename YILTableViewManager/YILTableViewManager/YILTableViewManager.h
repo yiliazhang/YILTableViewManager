da771876fa1e6e22fe04448fa6c04543f906ad9c
yilia.zhang <yilia_zhang_0825@163.com>
2018-05-25 11:50:28 +0800

    初始版本

---
+++ YILTableViewManager/YILTableViewManager/YILTableViewManager.h
@@ -0,0 +1,19 @@
+//
+//  YILTableViewManager.h
+//  YILTableViewManager
+//
+//  Created by apple on 2018/5/25.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YILTableViewManager.
+FOUNDATION_EXPORT double YILTableViewManagerVersionNumber;
+
+//! Project version string for YILTableViewManager.
+FOUNDATION_EXPORT const unsigned char YILTableViewManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YILTableViewManager/PublicHeader.h>
+
+
